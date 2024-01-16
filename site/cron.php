dbee103e06523d617c10b957b1acc1e1396377d7
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-16 09:12:06 +0400

    update

--- site/cron.php
+++ site/cron.php
@@ -1,5 +1,8 @@
 <?php
 define('MODX_API_MODE', true);
+define('MODX_BASE_PATH', dirname(__FILE__) . "/");
+define('MODX_SITE_URL', 'https://mailsend.skat59.ru/');
+define('MODX_BASE_URL', 'https://mailsend.skat59.ru/');
 include_once(dirname(__FILE__) . "/index.php");
 $modx->db->connect();
 if (empty($modx->config)) {

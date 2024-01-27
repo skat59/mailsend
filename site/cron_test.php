cfd20252b297d39e6b202685c781f9286bdd623c
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-27 17:13:19 +0400

    update

--- site/cron_test.php
+++ site/cron_test.php
@@ -6,12 +6,15 @@ header('HTTP/1.0 404 Not Found');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
 $dir = str_replace('\\','/',dirname(__FILE__)) . "/";
 
-define('MODX_API_MODE',  true);
-define('MODX_BASE_PATH', $dir);
-define('MODX_SITE_URL',  'https://mailsend.skat59.ru/');
-define('MODX_BASE_URL',  'https://mailsend.skat59.ru/');
+define('MODX_API_MODE',      true);
+define('MODX_BASE_PATH',     $dir);
+define('MODX_SITE_URL',      'https://mailsend.skat59.ru/');
+define('MODX_BASE_URL',      'https://mailsend.skat59.ru/');
+define('PARENT_SITR_URL',    'https://www.skat59.ru/');
+define('TITLE_PARENT',       'ООО «СКАТ» - надёжный поставщик спецтехники на Западном Урале');
 
 $dir = str_replace('\\','/',dirname(__FILE__)) . '/';
 
@@ -57,16 +60,17 @@ function parseContentMsg($content) {
 	foreach($matches as $arr):
 		$uid = gen_token($arr[1]);
 		$subst = "cid:" . $uid;
-		$re = "%" . $arr[1] . "%Usi";
-		$arr[] = pathinfo($arr[1], PATHINFO_BASENAME);
+		$re = "/" . preg_quote($arr[1], '/') . "/Usi";
+		/** Порядок наполнения массива строгий **/
 		$arr[] = $uid;
+		$arr[] = pathinfo($arr[1], PATHINFO_BASENAME);
 		$content = preg_replace($re, $subst, $content);
 		$return[] = $arr;
 	endforeach;
 	$content = $content . "\n" . '<p style="text-align: center;">Телефон для обратной связи: +7(342)270-00-10 доб. 3005
 <br>Или просто напишите нам: <a href="mailto:ofis@skat59.ru">ofis@skat59.ru</a></p>
 <p>&nbsp;</p>
-<p style="text-align: right;"><b>С огромным уважением к Вам<br /> &nbsp;<a href="https://www.skat59.ru/" target="_blank">Компания ООО «СКАТ»</a></b></p';
+<p style="text-align: right;"><b>С огромным уважением к Вам<br /> &nbsp;<a href="' . PARENT_SITR_URL . '" target="_blank">Компания ООО «СКАТ»</a></b></p';
 	$text = strip_tags($content);
 	$text = preg_replace('/([\r\n]+(?:\s+)?)/m', "\n", preg_replace('/(&nbsp;| )+/', " ", $text));
 	$arr_return = array(
@@ -87,10 +91,10 @@ $current = strtotime(date("d-m-Y 0:00:00", $time));
 $next = strtotime(date('d-m-Y 0:00:00', $current + 86400));
 
 // выбрать нужное сообщение, заголовок, файлы, дату отправки
+// Выбираем только один документ
 $evoPage = $modx->runSnippet('DocLister',
 	array(
 		'parents'           => '9',
-		// Выбираем только один документ
 		'display'           => '1',
 		'tvList'            => 'date_send,groups_send,files',
 		'tvPrefix'          => '',
@@ -103,6 +107,7 @@ $evoPage = $modx->runSnippet('DocLister',
 		'filters'           => 'AND(tv:date_send:egt:' . $current . ';tv:date_send:elt:' . $next . ')'
 	)
 );
+
 $evoDocument = json_decode($evoPage, true);
 $evoPage = json_encode($evoDocument, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 
@@ -134,17 +139,10 @@ if($evoDocument["rows"]):
 	endforeach;
 endif;
 
-// Выбор пользователей
 /*
 ------------------------------------
 -- Выбрать по определённой группе --
 ------------------------------------
-"SELECT * FROM $table WHERE (`groups` LIKE '$idGroup,%' OR `groups` LIKE '%,$idGroup' OR `groups` LIKE '%,$idGroup,%' OR `groups`='$idGroup') ORDER BY `id` ASC";
-*/
-
-// Выбрать из таблицы адреса для отправки
-// Перед рассылкой раскомментировать
-
 $table = $modx->getFullTableName( 'mailsend_users' );
 
 // Выбор группы
@@ -156,6 +154,7 @@ while( $row = $modx->db->getRow( $result ) ) {
 	$usr = json_decode(json_encode($row), false);
 	$mailArray[] = $usr;
 }
+*/
 
 echo "START" . PHP_EOL . str_pad("-", $pad, "-", STR_PAD_RIGHT) . PHP_EOL;
 
@@ -163,11 +162,11 @@ if($content_arr):
 
 	$messageTitle = $content_arr["title"];
 
-	$messageOut = '<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;margin-bottom:20px;max-width:100%;min-width:100%;width:100%"><tbody><tr style="background:#002952;color:#ffffff;font-size:16px;padding:15px;"><td style="background:#002952;color:#ffffff;font-size:16px;padding:15px;"><img style="display:inline-block;vertical-align:middle;width:100px" src="cid:logo_2u" /></td><td style="background:#002952;color:#ffffff;font-size:16px;padding:15px;width:100%!important;"><p style="display:inline-block;vertical-align:middle;width:100%;">ООО «СКАТ» - надёжный поставщик спецтехники на Западном Урале</p></td></tr><tr><td colspan="2">
+	$messageOut = '<table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;margin-bottom:20px;max-width:100%;min-width:100%;width:100%"><tbody><tr style="background:#002952;color:#ffffff;font-size:16px;padding:15px;"><td style="background:#002952;color:#ffffff;font-size:16px;padding:15px;"><img style="display:inline-block;vertical-align:middle;width:100px" src="cid:logo_2u" /></td><td style="background:#002952;color:#ffffff;font-size:16px;padding:15px;width:100%!important;"><p style="display:inline-block;vertical-align:middle;width:100%;">' . TITLE_PARENT . '</p></td></tr><tr><td colspan="2">
 	<!-- // -->
 	' . $content_arr["content"] . '
 	<!-- // -->
-	</td></tr><tr><td colspan="2" style="text-align: center; font-size: 10px !important;"><p style="text-align: center; font-size: 10px !important;">Вы можете отписаться от нашей рассылки.<br /><a href="https://mailsend.skat59.ru/unsubscribe/?token=%token%" target="_blank">Отписаться</a></p></td></tr></tbody></table>';
+	</td></tr><tr><td colspan="2" style="text-align: center; font-size: 10px !important;"><p style="text-align: center; font-size: 10px !important;">Вы можете отписаться от нашей рассылки.<br /><a href="' . MODX_SITE_URL . 'unsubscribe/?token=%token%" target="_blank">Отписаться</a></p></td></tr></tbody></table>';
 	$unsub = '<a href="' . MODX_SITE_URL . 'unsubscribe/?token=%token%" target="_blank">UNSUBSCRIBE</a>';
 
 	$messageID = 0;
@@ -187,6 +186,7 @@ if($content_arr):
 			$mailer->isSMTP();
 			$mailer->Encoding = $mailer::ENCODING_8BIT;
 			$mailer->CharSet = $mailer::CHARSET_UTF8;
+			// SMTP settings
 			$mailer->Mailer = 'smtp';
 			$mailer->SMTPAuth = true;
 			$mailer->Port = 465;

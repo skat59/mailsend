5518b61968c87640e57e15409307d2d445569b26
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-28 18:01:59 +0400

    update

--- site/assets/modules/MailSend/tpl/.default.php
+++ site/assets/modules/MailSend/tpl/.default.php
@@ -17,9 +17,7 @@ $start_link = $modx->config["site_manager_url"] . 'index.php?a=112&id=' . $conte
 <h1 class="d-none"><i class="fa fa-file-text"></i><?= $content["name"]; ?></h1>
 <div id="actions">
     <div class="btn-group">
-        <a id="Button1" class="btn btn-success" href="javascript:;" onclick="window.location.href='index.php?a=106';">
-            <i class="fa fa-times-circle"></i><span><?= $_lang["mailsend.close"]; ?></span>
-        </a>
+        <a id="Button1" class="b-none" href="javascript:;" onclick="return false;"></a>
     </div>
 </div>
 <div class="container-fluid">

1545620f1b41515fccb4bfa612832e3834db6b33
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-28 15:04:45 +0400

    Add MailSend

---
+++ site/assets/modules/MailSend/MailSend.module.php
@@ -0,0 +1,46 @@
+<?php
+if(IN_MANAGER_MODE!='true' && !$modx->hasPermission('exec_module')):
+	http_response_code(403);
+	die('For');
+endif;
+
+define('MODX_MAILSEND_PATH', str_replace(MODX_BASE_PATH, '', str_replace('\\', '/', realpath(dirname(__FILE__)))) . '/');
+define('MODX_MAILSEND_BASE_PATH', MODX_BASE_PATH . MODX_MAILSEND_PATH);
+
+$action = isset($_REQUEST['action']) ? trim(strip_tags($_REQUEST['action'])) : null;
+$page = (isset($_REQUEST['page']) && (int)$_REQUEST['page'] > 0) ? (int)$_REQUEST['page'] : 0;
+$a = (isset($_REQUEST['a']) && (int)$_REQUEST['a'] > 0) ? (int)$_REQUEST['a'] : 0;
+$formid = (isset($_REQUEST['formid']) && (int)$_REQUEST['formid'] > 0) ? (int)$_REQUEST['formid'] : 0;
+
+function getLang()
+{
+	global $_lang;
+	$modx = evolutionCMS();
+	$lang_path = MODX_MAILSEND_BASE_PATH . "lang/";
+	$userId = $modx->getLoginUserID();
+	if (!empty($userId)):
+		$lang = $modx->db->select('setting_value', $modx->getFullTableName('user_settings'), "setting_name='manager_language' AND user='{$userId}'");
+		if ($lng = $modx->db->getValue($lang)):
+			$managerLanguage = $lng;
+		endif;
+	endif;
+	include MODX_MANAGER_PATH.'includes/lang/english.inc.php';
+	if($managerLanguage != 'english'):
+		if (file_exists(MODX_MANAGER_PATH.'includes/lang/'.$managerLanguage.'.inc.php')):
+			include MODX_MANAGER_PATH.'includes/lang/'.$managerLanguage.'.inc.php';
+		endif;
+	endif;
+	include $lang_path . 'english.php';
+	if($managerLanguage != 'english'):
+		if (file_exists($lang_path . $managerLanguage.'.php')):
+			include $lang_path . $managerLanguage.'.php';
+		endif;
+	endif;
+	$_lang = array_merge($_lang, $_MailSendLang);
+}
+
+getLang();
+
+include_once MODX_MANAGER_PATH . 'includes/header.inc.php';
+echo '<pre><code>' . print_r($_lang, true) . '</code></pre>';
+include_once MODX_MANAGER_PATH . 'includes/footer.inc.php';
---
+++ site/assets/modules/MailSend/lang/russian-UTF8.php
@@ -0,0 +1,5 @@
+<?php
+
+$_MailSendLang = array(
+	
+);

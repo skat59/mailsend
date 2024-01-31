e6777d763e956a7b7988293b737158ef95f1956b
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2024-01-31 11:17:08 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,11 +197,11 @@ Sitemap: ${port}://${domain}/sitemap.xml
 					"bower_components/fancybox/src/js/thumbs.js",
 					"bower_components/fancybox/src/js/hash.js",
 					"bower_components/fancybox/src/js/wheel.js",
-					'bower_components/slick-carousel/slick/slick.js',
-					'bower_components/js-cookie/src/js.cookie.js',
-					'bower_components/jquery.cookie/jquery.cookie.js',
-					'bower_components/button-visually-impaired-javascript/dist/js/bvi.js',
-					"src/js/prism.js",
+					//'bower_components/slick-carousel/slick/slick.js',
+					//'bower_components/js-cookie/src/js.cookie.js',
+					//'bower_components/jquery.cookie/jquery.cookie.js',
+					//'bower_components/button-visually-impaired-javascript/dist/js/bvi.js',
+					//"src/js/prism.js",
 				],
 				dest: 'test/js/app.js'
 			},
@@ -310,7 +310,7 @@ Sitemap: ${port}://${domain}/sitemap.xml
 				files : {
 					'test/css/main.css' : [
 						'src/less/main.less',
-						'src/less/plugins/prism.css'
+						//'src/less/plugins/prism.css'
 					],
 					'test/css/tinymce.css' : [
 						'src/less/tinymce.less'
